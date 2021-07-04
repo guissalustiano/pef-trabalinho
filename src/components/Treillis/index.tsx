9cb0b75a8fb317072a781464799d062616a82044
Guilherme Salustiano <guissalustiano@gmail.com>
2021-07-04 01:34:43 -0300

    implement vierw calculated forces

--- src/components/Treillis/index.tsx
+++ src/components/Treillis/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Stage, Layer, Circle, Line, Arrow } from 'react-konva';
+import { Stage, Layer, Circle, Line, Arrow, Text } from 'react-konva';
 import { generateTreillisSample } from './data';
 import { solveTreillis } from "../../helper/treillis";
 
@@ -15,10 +15,12 @@ const initialState = solveTreillis(generateTreillisSample());
 const Drawer = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [treillis, setTreillis] = React.useState(initialState);
+  const [text, setText] = React.useState("batata");
   // const setAndCalcTreillis = (treillis: MutableUnweightedGraph<TreillisNode, TreillisEdge>) => setTreillis(solveTreillis(treillis));
 
   return (
     <Stage width={width} height={height} className="Drawer-wrapper">
+      <Layer><Text text={text} fontSize={32}/></Layer>
       <Layer offsetX={-width / 2} offsetY={-height / 2}>
         {treillis.nodes().filter(node => node.force !== undefined).map(node => (
           <Arrow
@@ -36,7 +38,9 @@ const Drawer = () => {
           return (
             <Line
               key={Math.random()} // TODO: fix this
+              onMouseOver={() => setText(`tração: ${edge.value.forceModule.toFixed(2)}`)}
               points={[pos1.x, pos1.y, pos2.x, pos2.y].map(x => x * posFactor)}
+              strokeWidth={5}
               stroke='black'
             />
           );
@@ -46,6 +50,7 @@ const Drawer = () => {
           return (
             <Circle
               key={Math.random()}
+              onMouseOver={() => setText(`fx: ${node.hiddenForce?.x?.toFixed() ?? 0} | fy: ${node.hiddenForce?.y?.toFixed() ?? 0}`)}
               x={node.pos.x * posFactor}
               y={node.pos.y * posFactor}
               radius={10}
