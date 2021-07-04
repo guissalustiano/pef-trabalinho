0c176a4d108643fe83ad5d8309519e6ef9f87b8f
Guilherme Salustiano <guissalustiano@gmail.com>
2021-07-04 01:16:33 -0300

    refa: refactory trellisSolver

--- src/components/Treillis/data.ts
+++ src/components/Treillis/data.ts
@@ -1,10 +1,12 @@
-import { UndirectedGraph } from "graphology";
-import { TreillisNode } from "../../helper/treillis";
+import { Graph } from "graphs-for-js";
+import { TreillisEdge, TreillisNode } from "../../helper/treillis";
 
 export const generateTreillisSample = () => {
-  const graph = new UndirectedGraph<TreillisNode>();
+  const graph = new Graph<TreillisNode, TreillisEdge>()
+  .keyFn(i => i.id).undirected.weighted()
 
-  graph.addNode('A', {
+  const nodeA = {
+    id: 'A',
     pos: {
       x: 2.5,
       y: -2.5
@@ -13,35 +15,45 @@ export const generateTreillisSample = () => {
       x: 5,
       y: 10,
     }
-  });
-  graph.addNode('B', {
+  }
+  const nodeB = {
+    id: 'B',
     pos: {
       x: -2.5,
       y: -2.5
     },
     link: { y: true }
-  });
-  graph.addNode('C', {
+  };
+  const nodeC = {
+    id: 'C',
     pos: {
       x: -2.5,
       y: 2.5
     },
     link: { x: true, y: true }
-  });
-  graph.addNode('D', {
+  };
+  const nodeD = {
+    id: 'D',
     pos: {
       x: 2.5,
       y: 2.5
     },
     link: { x: true, y: true }
-  });
+  };
 
-  graph.addEdge('B', 'A');
-  graph.addEdge('D', 'A');
-  graph.addEdge('C', 'D');
-  graph.addEdge('C', 'B');
-  graph.addEdge('C', 'A');
-  graph.addEdge('D', 'B');
+  graph.insert(nodeA)
+  graph.insert(nodeB)
+  graph.insert(nodeC)
+  graph.insert(nodeD)
+
+  graph.connect(nodeB, nodeA, {});
+  graph.connect(nodeD, nodeA, {});
+  graph.connect(nodeC, nodeD, {});
+  graph.connect(nodeC, nodeB, {});
+  graph.connect(nodeC, nodeA, {});
+  graph.connect(nodeD, nodeB, {});
+
+  console.log(graph)
 
   return graph
 }
--- src/components/Treillis/index.tsx
+++ src/components/Treillis/index.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 import { Stage, Layer, Circle, Line, Arrow } from 'react-konva';
 import { generateTreillisSample } from './data';
 import { solveTreillis } from "../../helper/treillis";
-import { TreillisNode } from "../../helper/treillis";
-import { UndirectedGraph } from "graphology";
 
-
-import "./style.css"
-import { NodeKey } from 'graphology-types';
-import Konva from 'konva';
+import "./style.css";
 
 const width = 700;
 const height = 500;
@@ -18,24 +13,16 @@ const forceFactor = 16;
 const initialState = solveTreillis(generateTreillisSample());
 
 const Drawer = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [treillis, setTreillis] = React.useState(initialState);
-  const setAndCalcTreillis = (treillis: UndirectedGraph<TreillisNode>) => setTreillis(solveTreillis(treillis));
+  // const setAndCalcTreillis = (treillis: MutableUnweightedGraph<TreillisNode, TreillisEdge>) => setTreillis(solveTreillis(treillis));
 
-  const handleNodeDrag = (nodeId: NodeKey, e: Konva.KonvaEventObject<DragEvent>) => {
-    treillis.mergeNodeAttributes(nodeId, { pos: { x: e.target.x()/posFactor, y: e.target.y()/posFactor } });
-
-    // Quando setamos a mesma referencia o setState acha que nada mudou por isso nao desenha o componente novamente, mas os valores mudaram
-    // para resolver isso estamos copiando e gerando uma nova treliça, entretanto acabamos gerando um novo elemento de forma desnecessaria
-    setAndCalcTreillis(treillis.copy()); //TODO: improve
-  };
-
-  const nodes = Array.from(treillis.nodeEntries()).map(([id, atributes]) => ({ id, ...atributes }));
   return (
     <Stage width={width} height={height} className="Drawer-wrapper">
       <Layer offsetX={-width / 2} offsetY={-height / 2}>
-        {nodes.filter(node => node.force !== undefined).map(node => (
+        {treillis.nodes().filter(node => node.force !== undefined).map(node => (
           <Arrow
-            key={node.id}
+            key={Math.random()}
             x={node.pos.x * posFactor}
             y={node.pos.y * posFactor}
             points={[0, 0, node!.force!.x, node!.force!.y].map(x => x * forceFactor)}
@@ -44,26 +31,23 @@ const Drawer = () => {
           />))}
 
         {treillis.edges().map((edge) => {
-          const [node1, node2] = treillis.extremities(edge)
-          const pos1 = treillis.getNodeAttributes(node1).pos
-          const pos2 = treillis.getNodeAttributes(node2).pos
+          const pos1 = edge.source.pos
+          const pos2 = edge.target.pos
           return (
             <Line
-              key={edge}
+              key={Math.random()} // TODO: fix this
               points={[pos1.x, pos1.y, pos2.x, pos2.y].map(x => x * posFactor)}
               stroke='black'
             />
           );
         })}
 
-        {nodes.map(node => {
+        {treillis.nodes().map(node => {
           return (
             <Circle
-              key={node.id} id={node.id}
+              key={Math.random()}
               x={node.pos.x * posFactor}
               y={node.pos.y * posFactor}
-              draggable
-              onDragMove={(e) => handleNodeDrag(node.id, e)}
               radius={10}
               fill="red" />
           );
