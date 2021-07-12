2781519dc2ed02339626ef51c8f4c8e446bba0c9
Guilherme Salustiano <guissalustiano@gmail.com>
2021-07-12 02:22:44 -0300

    feat: add zoom

--- src/components/TreillisView/index.tsx
+++ src/components/TreillisView/index.tsx
@@ -6,6 +6,8 @@ import "./style.css";
 import TreillisViewNode from '../TreillisViewNode';
 import TreillisViewEdge from '../TreillisViewEdge';
 import TreillisViewForce from '../TreillisViewForce';
+import { useState } from 'react';
+import { KonvaEventObject } from 'konva/lib/Node';
 
 type TreillisViewParams = {
   treillis: MutableWeightedGraph<TreillisNode, TreillisEdge>,
@@ -16,12 +18,47 @@ type TreillisViewParams = {
 const TreillisView = ({ treillis, width, height }: TreillisViewParams) => {
   const widthFinal = width ?? window.innerWidth;
   const heightFinal = height ?? window.innerHeight;
-  const scale = 50;
+
   const maxEdgeforce = Math.max(...treillis.edges().map(e => e.value.forceModule))
 
+  const [scale, setScale] = useState(50);
+  const [position, setPosition] = useState({x: widthFinal/2, y: heightFinal/2})
+  const scaleBy = 1.01;
+
+  const onWhellScale = (e: KonvaEventObject<WheelEvent>) => {
+    e.evt.preventDefault();
+
+    const stage = e.target.getStage();
+    const oldScale = stage.scaleX();
+    const mousePointTo = {
+      x: stage.getPointerPosition().x / oldScale - stage.x() / oldScale,
+      y: stage.getPointerPosition().y / oldScale - stage.y() / oldScale
+    };
+
+    const newScale = e.evt.deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+
+    setScale(newScale);
+
+    var newPos = {
+      x: -(mousePointTo.x - stage.getPointerPosition().x / newScale) * newScale,
+      y: -(mousePointTo.y - stage.getPointerPosition().y / newScale) * newScale,
+    };
+    
+    setPosition(newPos);
+  }
+
   return (
-    <Stage width={widthFinal} height={heightFinal} className="treillis-view-wrapper" scaleX={scale} scaleY={scale}>
-      <Layer offsetX={-5} offsetY={-5}>
+    <Stage
+      width={widthFinal}
+      height={heightFinal}
+      className="treillis-view-wrapper"
+      scaleX={scale}
+      scaleY={scale}
+      onWheel={onWhellScale}
+      x={position.x}
+      y={position.y}
+      >
+      <Layer>
         {treillis.nodes().filter(node => node.force !== undefined).map(node => (
           <TreillisViewForce node={node} scale={scale} />))}
 
