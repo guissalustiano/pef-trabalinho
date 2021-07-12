ca8b0ba42b1a42e396e1c9a1a42abd9cfd231749
Guilherme Salustiano <guissalustiano@gmail.com>
2021-07-12 03:03:57 -0300

    add node names

--- src/components/TreillisViewNode/index.tsx
+++ src/components/TreillisViewNode/index.tsx
@@ -1,4 +1,4 @@
-import { Circle } from 'react-konva';
+import { Circle, Text } from 'react-konva';
 import { TreillisNode } from "../../helper/treillis";
 
 import "./style.css";
@@ -8,14 +8,24 @@ type TreillisViewNodeParams = {
   scale?: number,
 }
 
-const TreillisViewNode = ({ node, scale=1 }: TreillisViewNodeParams) => (
-  <Circle
-    key={node.id}
-    x={node.pos.x }
-    y={node.pos.y}
-    radius={10/scale}
-    fill="purple" />
-)
+const TreillisViewNode = ({ node, scale=1 }: TreillisViewNodeParams) => {
+  const r = 10 / scale;
+  return (<>
+    <Circle
+      key={node.id}
+      x={node.pos.x}
+      y={node.pos.y}
+      radius={r}
+      fill="purple" />
+
+    <Text
+        x={node.pos.x - 3*r}
+        y={node.pos.y - 3*r}
+        text={node.id}
+        fontSize={32/scale}/>
+    </>
+  );
+}
 
 
 
